13e363a65e160b1b21b1c2edd4c84aadd5bebc0a
SiddharthaAnand <siddharthalibra13@gmail.com>
2017-10-28 14:25:44 +0530

    Activate Item Pipeline

--- coauthornetwork/settings.py
+++ coauthornetwork/settings.py
@@ -64,9 +64,9 @@ DOWNLOAD_DELAY = 2
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'coauthornetwork.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    "coauthornetwork.pipelines.CoauthorNetworkPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
