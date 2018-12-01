c2a5a905a69434dcecbbc41db4fab270c346c5ed
Siddhartha Anand <siddharthalibra13@gmail.com>
2018-12-01 18:49:09 +0530

    Add pipeling to insert into mongodb

--- coauthornetwork/pipelines.py
+++ coauthornetwork/pipelines.py
@@ -5,12 +5,38 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exceptions import DropItem
-
+import pymongo
+import read_credential
 
 class CoauthorNetworkPipeline(object):
 
+    collection_name = 'testCollection'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.username, self.pwd, self.url = read_credential.read_credential("/Users/sianand/github/dblp-spider/coauthornetwork/credentials")
+        self.mongo_uri = mongo_uri.format(username=self.username, pwd=self.pwd)
+        self.mongo_db = mongo_db
+        self.db = None
+        self.client = None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
     def process_item(self, item, spider):
-        if (item["author_name"] is not None or item["author_name"] != ""):
+        try:
+            self.db[self.collection_name].insert_one(dict(item))
             return item
-        else:
-            raise DropItem("Missing author name in %s" % item)
+        except Exception:
+            raise DropItem()
+
+    def close_spider(self, item, spider):
+        self.client.close()
